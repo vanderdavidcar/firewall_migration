cb432683437531cbb93e953cd0acba534a4a987d
vanderson <vander_davidcar@yahoo.com.br>
2023-04-17 09:16:03 -0300

    remove rules

--- script_rules.py
+++ script_rules.py
@@ -1,35 +1,49 @@
 from netmiko import ConnectHandler
 import dev_connection
+import re
 from dotenv import load_dotenv
 from datetime import datetime
 load_dotenv()
 
 start_time = datetime.now()
 
-# prefix group name of object "addgr" to use for loop
-objects = ["fontnet", "redes"]
-
 """
 Call function dev_connection that have all device and user information to connect and collect
 """
 net_connect = ConnectHandler(**dev_connection.iosv)
 net_connect.enable()
-cmd = net_connect.send_command(f'show run access-list | in PACI')
+cmd = net_connect.send_command(f'show run access-list | in INBURSA')
 print(cmd)
+rules = cmd.splitlines()
 
+untrustintf = 'UNTRUST-DCV (DCV-1/39.1505)'
 """
 Creation rules UNTRUST
 """
 # Create Policy objects 
 print('config firewall policy')
-for i in objects:
-    print('edit 0')
-    print('set srcintf "DCV"')
-    print(f'set dstintf "{i.upper()}"')
-    print('set srcaddr "all"')
-    print(f'set dstaddr "{i.upper()}"')
-    print('set action accept')
-    print('set schedule "always"')
-    print('set service "ALL"')
-    print('set fsso disable')
-    print('next')
+for i in rules:
+    """
+    Regex pattern to find exatly subnets
+    """
+    # Pattern to find only static routes on routing table 
+    pattern = re.compile(r"access-list (?P<name_rule>\S+) extended (?=.*\bpermit\b)(\w+ )(\w+ )(?P<srcaddr>\w+ )(\w+\S+ )(?P<dstaddr>\w+)")
+    match = pattern.search(cmd)
+    name_rule = match.group("name_rule")
+    srcaddr = match.group("srcaddr") 
+    dstaddr = match.group("dstaddr") 
+    match = re.findall(pattern, cmd)
+
+    if dstaddr in i:
+        print('edit 0')
+        print(f'set name "{name_rule}"')
+        print('set srcintf "DCV"')
+        print(f'set dstintf "{untrustintf}"')
+        print(f'set srcaddr "ALL"')
+        print(f'set dstaddr "{dstaddr}"')
+        print('set action accept')
+        print('set schedule "always"')
+        print('set service "ALL"')
+        print('set fsso disable')
+        print('next')
+print('end')
